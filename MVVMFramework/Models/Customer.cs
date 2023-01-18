dc724a629d7158e99f8191b624a35b024bec73d7
kuro <kuro78@gmail.com>
2023-01-18 22:23:09 +0900

    유효성 검사 코드 추가

--- MVVMFramework/Models/Customer.cs
+++ MVVMFramework/Models/Customer.cs
@@ -1,31 +1,102 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVVMFramework.Models;
 
 /// <summary>
 /// Customer 모델
 /// </summary>
-public partial class Customer : ObservableObject
+public class Customer : ObservableValidator
 {
-    public string CustomerID;
-    [ObservableProperty]
+    private string _CustomerID;
+    [Required]
+    [MaxLength(5)]
+    public string CustomerID
+    {
+        get { return _CustomerID; }
+        set { SetProperty(ref _CustomerID, value, true); }
+    }
+    
     private string _CompanyName;
-    [ObservableProperty]
+    [Required]
+    [MaxLength(40)]
+    public string CompanyName
+    {
+        get { return _CompanyName; }
+        set { SetProperty(ref _CompanyName, value, true); }
+    }
+    
     private string _ContactName;
-    [ObservableProperty]
+    [MaxLength(30)]
+    public string ContactName
+    {
+        get { return _ContactName; }
+        set { SetProperty(ref _ContactName, value, true); }
+    }
+
     private string _ContactTitle;
-    [ObservableProperty]
+    [MaxLength(30)]
+    public string ContactTitle
+    {
+        get { return _ContactTitle; }
+        set { SetProperty(ref _ContactTitle, value, true); }
+    }
+
     private string _Address;
-    [ObservableProperty]
+    [MaxLength(60)]
+    public string Address
+    {
+        get { return _Address; }
+        set { SetProperty(ref _Address, value, true); }
+    }
+
     private string _City;
-    [ObservableProperty]
+    [MaxLength(15)]
+    public string City
+    {
+        get { return _City; }
+        set { SetProperty(ref _City, value, true); }
+    }
+    
     private string _Region;
-    [ObservableProperty]
+    [MaxLength(15)]
+    public string Region
+    {
+        get { return _Region; }
+        set { SetProperty(ref _Region, value, true); }
+    }
+    
     private string _PostalCode;
-    [ObservableProperty]
+    [MaxLength(10)]
+    public string PostalCode
+    {
+        get { return _PostalCode; }
+        set { SetProperty(ref _PostalCode, value, true); }
+    }
+    
     private string _Country;
-    [ObservableProperty]
+    [MaxLength(15)]
+    public string Country
+    {
+        get { return _Country; }
+        set { SetProperty(ref _Country, value, true); }
+    }
+    
     private string _Phone;
-    [ObservableProperty]
+    [MaxLength(24)]
+    [Phone]
+    public string Phone
+    {
+        get { return _Phone; }
+        set { SetProperty(ref _Phone, value, true); }
+    }
+    
     private string _Fax;
+    [MaxLength(24)]
+    [Phone]
+    public string Fax
+    {
+        get { return _Fax; }
+        set { SetProperty(ref _Fax, value, true); }
+    }
 }
--- MVVMFramework/ViewModels/CustomerViewModel.cs
+++ MVVMFramework/ViewModels/CustomerViewModel.cs
@@ -5,6 +5,8 @@ using MVVMFramework.Bases;
 using MVVMFramework.Models;
 using MVVMFramework.Services;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 
 namespace MVVMFramework.ViewModels;
@@ -17,7 +19,13 @@ public partial class CustomerViewModel : ViewModelBase
     [ObservableProperty]
     private IList<Customer> _Customers;
 
-    public ICommand BackCommand { get; set; }
+    [ObservableProperty]
+    private Customer _SelectedCustomer;
+
+    [ObservableProperty]
+    private string _ErrorMessage;
+
+    public IRelayCommand SaveCommand { get; set; }
 
     public CustomerViewModel(IDatabaseService databaseService) 
     {
@@ -28,11 +36,82 @@ public partial class CustomerViewModel : ViewModelBase
     private void Init()
     {
         Title = "Customer";
-        BackCommand = new RelayCommand(OnBack);
+        
+        SaveCommand = new RelayCommand(Save,
+                        () => Customers != null
+                        && Customers.Any(c => string.IsNullOrWhiteSpace(c?.CustomerID)));
+
+        PropertyChanging += CustomerViewModel_PropertyChanging;
+        PropertyChanged += CustomerViewModel_PropertyChanged;
     }
 
+    private void CustomerViewModel_PropertyChanging(object sender, System.ComponentModel.PropertyChangingEventArgs e)
+    {
+        switch(e.PropertyName)
+        {
+            case nameof(SelectedCustomer):
+                if (SelectedCustomer != null)
+                {
+                    SelectedCustomer.ErrorsChanged -= SelectedCustomer_ErrorChanged;
+                    ErrorMessage = string.Empty;
+                }
+                break;
+        }
+    }
 
-    private void OnBack()
+    private void CustomerViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        switch(e.PropertyName)
+        {
+            case nameof(SelectedCustomer):
+                if (SelectedCustomer != null)
+                {
+                    SelectedCustomer.ErrorsChanged += SelectedCustomer_ErrorChanged;
+                    SetErrorMessage(SelectedCustomer);
+                }
+                break;
+        }
+    }
+
+    private void SelectedCustomer_ErrorChanged(object sender, System.ComponentModel.DataErrorsChangedEventArgs e)
+    {
+        SetErrorMessage(sender as Customer);
+    }
+
+    private void SetErrorMessage(Customer customer)
+    {
+        if (customer == null)
+        {
+            return;
+        }
+
+        var errors = customer.GetErrors();
+        ErrorMessage = string.Join("\n", errors.Select(e => e.ErrorMessage));
+    }
+
+    private void Save()
+    {
+        MessageBox.Show("Save");
+        SaveCommand.NotifyCanExecuteChanged();
+    }
+
+    /// <summary>
+    /// AddCommand
+    /// </summary>
+    [RelayCommand]
+    private void Add()
+    {
+        var newCustomer = new Customer();
+        Customers.Insert(0, newCustomer);
+        SelectedCustomer = newCustomer;
+        SaveCommand.NotifyCanExecuteChanged();
+    }
+
+    /// <summary>
+    /// BackCommand
+    /// </summary>
+    [RelayCommand]
+    private void Back()
     {
         WeakReferenceMessenger.Default.Send(new NavigationMessage("GoBack"));
     }
